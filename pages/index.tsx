e44f1497ef4c544d2eb5e32b172a8669fecf3bd7
ray609trade <140095350+ray609trade@users.noreply.github.com>
2025-06-02 07:26:48 -0400

    Add basic Next.js page component

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>My Next.js App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Welcome to Next.js!</h1>
+      </main>
+    </div>
+  )
+}
